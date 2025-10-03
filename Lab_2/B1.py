bf68d02acc1f7bf7951bcf973a5e80464dfb3301
Dergavko Dominika <domi.der08@gmail.com>
2025-10-03 12:04:36 +0300

    сделано задание B1

---
+++ Lab_2/B1.py
@@ -0,0 +1,20 @@
+for i in range(0,3):
+    total_dis = 0
+    file = open(f'inmap{i}.dat', 'r', encoding='utf-8')
+    mile = file.readlines(1)
+    a = mile[0].split(' ')
+    scale = float(a[2].strip())
+    print('Dominika Dergavko\nSimple Map Distance Computations\n')
+    print(f'Map Scale Factor:    {scale} miles per inch\n')
+    print('    Map        Mileage')
+    print('    Measure    Distance')
+    print('=============================================================')
+    for h in range(1, int(a[0])+1):
+        measure = file.readlines(2)
+        distance_0 = float(measure[0].strip())*scale
+        distance = round(distance_0,1)
+        total_dis+=distance
+        print(f'#  {h}           {distance}')
+    print('=============================================================')
+    print(f'Total Distance:   {total_dis}')
+    file.close()

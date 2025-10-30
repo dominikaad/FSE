4bb3f4783177fdfc470b3cd0e7fd1cf55bab8692
Dergavko Dominika <domi.der08@gmail.com>
2025-10-30 23:14:35 +0300

    нету макс и мин

--- Laba_4/B1.py
+++ Laba_4/B1.py
@@ -12,25 +12,18 @@ def write_inf(count_num, number, precipitation, len):
         if k in number:
             for t in range(0, len):
                 if k ==number[t]:
-                    answ_file.write(f'{str(k)}\t\t{str(precipitation[t])}\n')
+                    if precipitation[t] != 0.00:
+                        count_star = '*'*int((precipitation[t]//0.25)+1)
+                        answ_file.write(f'{str(k)}\t\t{precipitation[t]:.2f} {count_star}\n')
+                    else:
+                        answ_file.write(f'{str(k)}\t\t{precipitation[t]:.2f}\n')
                     break
         else:
             answ_file.write(f'{str(k)}\t\tNA\n')
 
 
 
-    # list_num_precip = {number[t]:
-    #                    precipitation[t] for t in range(len)}
-    # for k in range(1, count_num+1):
-    #     if k in list_num_precip:
-    #         answ_file.write(f'{k}\t\t{list_num_precip[k]}\n')
-    #     else:
-    #         answ_file.write(f'{str(k)}\t\tNA\n')
 
-
-flag_31 = False
-flag_30 = False
-flag_28 = False
 
 for i in range(0,3):
     if i == 0:
@@ -48,27 +41,25 @@ for i in range(0,3):
     place = information[1].strip()
     data = information[2].strip()
     month = data.split(',')[0]
-    flag_num = False
-    flag_replay = False
-
+    count_num_31 = 0
+    count_num_30 = 0
+    count_num_28 = 0
     array_num = []
     array_precip = []
     replay_num = 0
-
-
+    sum_precip = 0
 
     answ_file.write('Programmer: Dominika Dergavko\n')
     answ_file.write(f'{inf_project}\n')
     answ_file.write(f'Precipitation report for {place} during {data}\n')
     answ_file.write('Error\t\tDay\t\tLine\n')
 
-    if month == 'January' or month == 'March' or month == 'May' or month == 'July' or month == 'August' or month == 'October' or month == 'December':
-        flag_31 = True
-    elif month == 'April' or month == 'June' or month == 'September' or month == 'November':
-        flag_30 = True
-    elif month == 'February':
-        flag_28 = True
-
+    # if month == 'January' or month == 'March' or month == 'May' or month == 'July' or month == 'August' or month == 'October' or month == 'December':
+    #     flag_31 = True
+    # elif month == 'April' or month == 'June' or month == 'September' or month == 'November':
+    #     flag_30 = True
+    # elif month == 'February':
+    #     flag_28 = True
 
     for h in range(3, len_inf):
         if len(information[h].split(' '))==1:
@@ -89,43 +80,40 @@ for i in range(0,3):
         list_inf = Counter(array_num)
         for key, value in list_inf.items():
             if value==2:
-                flag_replay = True
-                if key==replay_num:
-                    flag_replay = False
-                else:
+                if key!=replay_num:
                     replay_num = key
                     write_error('R', key, h)
                     list_inf[key] = 1
                     print(list_inf)
-                    flag_replay = False
+                    # print(precipitation)
+                    # sum_precip += precipitation
 
         if month=='January' or month=='March' or month=='May' or month=='July' or month=='August' or month=='October' or month=='December':
+            count_num_31+=1
+            count_precip = 31
             if 0>number or number>31:
-                flag_num = True
                 write_error('N', number, h)
-                flag_num = False
         elif month=='April' or month=='June' or month=='September' or month=='November':
+            count_num_30+=1
+            count_precip = 30
             if 0>number or number>30:
-                flag_num = True
                 write_error('N', number, h)
-                flag_num = False
         elif month=='February':
+            count_num_28+=1
+            count_precip = 28
             if 0>number or number>28:
-                flag_num = True
                 write_error('N', number, h)
-                flag_num = False
-    if flag_30:
-        for key, value in list_num_precip.items():
-            write_inf(30, array_num, array_precip, len_array_num)
-            flag_30 = False
-    elif flag_31:
-        for key, value in list_num_precip.items():
-            write_inf(31, array_num, array_precip, len_array_num)
-            flag_31 = False
-    elif flag_28:
-        for key, value in list_num_precip.items():
-            write_inf(28, array_num, array_precip, len_array_num)
-            flag_28 = False
 
+    if count_num_30==len_inf-3:
+        write_inf(30, array_num, array_precip, len_array_num)
+    elif count_num_31 == len_inf - 3:
+        write_inf(31, array_num, array_precip, len_array_num)
+    elif count_num_28 == len_inf - 3:
+        write_inf(28, array_num, array_precip, len_array_num)
+    answ_file.write('Minimum\t\tMaximum\t\tAverage\n')
+    print(sum_precip)
+    print(count_precip)
+    average = sum_precip/count_precip
+    answ_file.write(str(format(average, ".2f")))
     answ_file.close()
     file.close()
